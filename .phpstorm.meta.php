1bc8005ffd464b5d5cfb6cb420f6c4b9f8557b5c
Carlos Clayton <carlos.clayton@gmail.com>
2019-11-23 14:13:49 -0300

    Using GuzzleHttp with Laravel

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-11-05 12:41:02.
+    * Generated on 2019-11-23 17:13:26.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
